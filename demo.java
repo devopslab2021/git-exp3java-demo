b9bbbd8d40c33bcaae9a14c4240ca7b0ce1e8b03
devopslab2021 <devopslab2021@gmail.com>
2022-02-21 11:48:58 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -1,5 +1,8 @@
 class demo{  
     public static void main(String args[]){  
+     System.out.println("Java Program");
+             System.out.println("Java Program");  
      System.out.println("Java Program");  
+     System.out.println("Java Program");
     }  
 }  
